8e1e273bd4c0bc40c6187f652d5b3e789a970c0b
philip loeffler <philiploeffler@gmail.com>
2020-08-18 19:09:22 -0400

    comment

--- bmdb/Capstone_BMDb_Create_v01_only_movie (1).sql
+++ bmdb/Capstone_BMDb_Create_v01_only_movie (1).sql
@@ -18,5 +18,12 @@ Director 	varchar(255) 	not null
  insert into Movie VALUES
  	(1, 'Star Wars', 1977, 'PG', 'George Lucas'),
  	(2, 'Sixteen Candles', 1984, 'PG', 'John Hughes'),
-  	(3, 'Fifth Element', 1997, 'PG-13', 'John Hughes'),
- 	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland');
+  	(3, 'Fifth Element', 1997, 'PG-13', 'luc Benson'),
+ 	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland'),
+	(5, 'Nightmare Before Christmas', 1993, 'PG', 'Tim Burton'),
+ 	(6, 'Hot Rod', 2007, 'PG-13', 'Akiva Schaffer'),
+ 	(7, 'Inside Out', 2015, 'PG', 'Pete Docter'),
+ 	(8, 'Pineapple Express', 2008, 'R', 'David Gordon Green'),
+	(9, 'Django Unchained', 2012, 'R', 'Quentin Tarantino'),
+	(10, 'Pulp Fiction', 1994, 'R', 'Quentin Tarantino');
+
