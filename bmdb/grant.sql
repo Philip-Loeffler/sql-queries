ab34c98f7720922497731dc0b99524c4cd797551
philip loeffler <philiploeffler@gmail.com>
2020-08-24 21:10:58 -0400

    grant

---
+++ bmdb/grant.sql
@@ -0,0 +1,3 @@
+DROP USER IF EXISTS bmdb_user@localhost;
+CREATE USER bmdb_user@localhost IDENTIFIED BY 'sesame';
+GRANT SELECT, INSERT, DELETE, UPDATE ON bmdb.* TO bmdb_user@localhost;
---
+++ bmdb/view.sql
@@ -0,0 +1,15 @@
+-- create / alter a view
+-- alter view movieCredits as 
+create view movieCredits as 
+select m.Title, m.Year, a.FirstName, a.LastName, c.Role 
+  from actor a
+  join credit c
+    on a.ID = c.ActorID
+  join movie m
+    on m.ID = c.MovieID;
+-- use the view
+select * from movieCredits;
+​
+-- use the view and order by
+select * from movieCredits
+ order by title;
