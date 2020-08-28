d3117bff67cf44382a525751e63f6d77428822a0
philip loeffler <philiploeffler@gmail.com>
2020-08-28 17:24:58 -0400

    sql

--- bmdb/prsCreateScript.sql
+++ bmdb/prsCreateScript.sql
@@ -6,7 +6,7 @@ Create table User (
 ID 				integer 		primary key auto_increment,
 UserName 		varchar(20) 	not null,
 Password 		varchar(10) 	not null,
-FirstName 		varchar(20) 	not null,
+FirstName 		varchar(100) 	not null,
 LastName 		varchar(20) 	not null,
 PhoneNumber 	varchar(12) 	not null,
 Email 			varchar(75) 	not null,
@@ -64,10 +64,9 @@ Foreign Key (ProductID) references Product(ID),
 Foreign Key (RequestID) references Request(ID)
 );
 
-
+-- cant get insert statement to run due to "data to long for column name" researching it said i need to take mysql off of "strict mode" could you try inserting and seeing if it works on your machine?
 INSERT into user (ID, username, password, firstname, lastname, phonenumber, email, isReviewer, isAdmin) VALUES
 (1, 'SYSTEM', 'xxxxx', 'System', 'System', 'XXX-XXX-XXXX', 'system@test.com', 0, 0);
-
 
 INSERT into vendor (ID, code, name, address, city, state, zip, phonenumber, email) VALUES
 ('1', 'BB-1001', 'Best Buy', '100 Best Buy Street', 'Louisville', 'KY', '40207', '502-111-9099', 'geeksquad@bestbuy.com'),
@@ -76,11 +75,11 @@ INSERT into vendor (ID, code, name, address, city, state, zip, phonenumber, emai
 ('4', 'ST-1001', 'Staples', '9550 mason montgomery rd', 'Mason', 'OH', '45040', '513-7540235', 'support@orders.staples.com'),
 ('5', 'MC-1001', 'Micro Center', '11755 mosteller rd', 'Sharonville', 'OH', '45241', '513-782-8500', 'support@microcenter.com');
 
-INSERT into product (ID,vendorID, partnumber, name, price, unit, PhotoPath)  VALUES
+INSERT into product (ID, vendorID, partnumber, name, price, unit, PhotoPath)  VALUES
 ('1', '1','ME280LL', 'iPad Mini 2', '296.99',NULL,NULL),
 ('2', '2','ME280LL', 'iPad Mini 2', '296.99', null,null),
-('3', '3', '105810', 'hammermill paper, premium multi-purpose paper polywrap', '8.99', null),
-('4', '4', '122374', 'hammermill copy plus copy paper, 81/2 x 11, case', '29.99', null),
+('3', '3', '105810', 'hammermill paper, premium multi-purpose paper polywrap', '8.99', null, null),
+('4', '4', '122374', 'hammermill copy plus copy paper, 81/2 x 11, case', '29.99',null,  null),
 ('5', '4', '784551', 'logitch m325 wireless optical mouse, ambidextrous, black', '14.99', null, null),
 ('6', '4','382955', 'staples mouse pad, black',  '2.99', null, null),
 ('7', '4','2122178', 'AOC 24-inch class LED Monitor', '99.99', null, null),
@@ -88,7 +87,7 @@ INSERT into product (ID,vendorID, partnumber, name, price, unit, PhotoPath)  VAL
 ('9', '4','2256788', 'Laptop Dell i3552-3240BLK 15.6',  '239.99', null, null),
 ('10', '4','IM12M9520','LapTop acer acer aspire one cloudbook 14', '224.99', null, null),
 ('11', '4','940600', 'cannon imageCLASS Copier (D530)',  '99.99', null, null),
-('12', '5','228148', 'Acer Aspire ATC-780a-UR12 Desktop Computer', '399.99'),
-('13', '5','279364', 'Lenovo ideaCentre All-in-one desktop', '349.99');
+('12', '5','228148', 'Acer Aspire ATC-780a-UR12 Desktop Computer', '399.99', null, null),
+('13', '5','279364', 'Lenovo ideaCentre All-in-one desktop', '349.99',null, null);
 
 
